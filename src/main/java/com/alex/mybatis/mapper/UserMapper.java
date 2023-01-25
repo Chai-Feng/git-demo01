3463d43e354d94313d72c1d885cb0e505a44bee2
Alex <1796794973@qq.com>
2023-01-25 14:18:12 +0800

    mybatis入门2

--- src/main/java/com/alex/mybatis/mapper/UserMapper.java
+++ src/main/java/com/alex/mybatis/mapper/UserMapper.java
@@ -1,5 +1,13 @@
 package com.alex.mybatis.mapper;
 
+import com.alex.mybatis.entity.User;
+import org.apache.ibatis.annotations.MapKey;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+import java.util.Map;
+
 /**
  * @Title:
  * @Description: TODO
@@ -13,4 +21,76 @@ public interface UserMapper {
      * 添加用户信息
      */
     int insertUser();
+
+
+
+    User getUserById(@Param("id1") int id);
+
+    /**
+     * 查询所有用户信息
+     * @return
+     */
+    List<User> getUserList();
+
+
+    /**
+     * 查询用户的总记录数
+     * @return
+     * 在MyBatis中，对于Java中常用的类型都设置了类型别名
+     * 例如： java.lang.Integer-->int|integer
+     * 例如： int-->_int|_integer
+     * 例如： Map-->map,List-->list
+     */
+    int getCount();
+
+    /**
+     * 根据用户id查询用户信息为map集合
+     * @param id
+     * @return
+     */
+    Map<String,Object> getUserToMap(@Param("id")int id);
+
+
+    /**
+     * 查询所有用户信息为map集合
+     * @return
+     * 将表中的数据以map集合的方式查询，一条数据对应一个map；若有多条数据，就会产生多个map集合，此
+    时可以将这些map放在一个list集合中获取
+     */
+    List<Map<String, Object>> getAllUserToMap();
+
+    /**
+     * 查询所有用户信息为map集合
+     * @return
+     * 将表中的数据以map集合的方式查询，一条数据对应一个map；若有多条数据，就会产生多个map集合，并
+    且最终要以一个map的方式返回数据，此时需要通过@MapKey注解设置map集合的键，值是每条数据所对应的
+    map集合
+     */
+    @MapKey("id")
+    Map<String,Object> getAllUserToMap2();
+
+    /**
+     * 模糊查询
+     * @param keyword
+     * @return
+     */
+    List<User> getMohu(@Param("keyword")String keyword);
+
+
+    /**
+     * 批量删除
+     * @param ids
+     * @return
+     */
+    int deleteMore(@Param("ids") String ids);
+
+
+    /**
+     * useGeneratedKeys：设置使用自增的主键
+     * keyProperty：因为增删改有统一的返回值是受影响的行数，因此只能将获取的自增的主键放在传输的参数user对象的某个属性中
+     * @param user
+     * @return
+     */
+    int insertUserGeneratedKeys(User user);
+
 }
--- src/test/java/com/alex/mybtais/MbTest.java
+++ src/test/java/com/alex/mybtais/MbTest.java
@@ -1,5 +1,6 @@
 package com.alex.mybtais;
 
+import com.alex.mybatis.entity.User;
 import com.alex.mybatis.mapper.UserMapper;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
@@ -9,6 +10,8 @@ import org.junit.Test;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @Title:
@@ -19,6 +22,22 @@ import java.io.InputStream;
  */
 public class MbTest {
 
+
+    public UserMapper getMapper(){
+        InputStream is = null;
+        UserMapper mapper =null;
+        try {
+            is = Resources.getResourceAsStream("mybatis-config.xml");
+            SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(is);
+
+            SqlSession sqlSession = factory.openSession(true);
+             mapper = sqlSession.getMapper(UserMapper.class);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return mapper;
+
+    }
     @Test
     public  void test1(){
         InputStream is=null;
@@ -53,4 +72,103 @@ public class MbTest {
             }
         }
     }
+
+    @Test
+    public void testSelect(){
+        try {
+            InputStream is = Resources.getResourceAsStream("mybatis-config.xml");
+            SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(is);
+
+            SqlSession sqlSession = factory.openSession(true);
+            UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+
+            User userById = mapper.getUserById(1);
+
+            System.out.println(userById);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    @Test
+    public void testSelectList(){
+        try {
+            InputStream is = Resources.getResourceAsStream("mybatis-config.xml");
+            SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(is);
+
+            SqlSession sqlSession = factory.openSession(true);
+            UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+
+            List<User> userList = mapper.getUserList();
+
+            System.out.println(userList);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+
+    @Test
+    public void testGetCount(){
+        UserMapper mapper = getMapper();
+        int count = mapper.getCount();
+        System.out.println(count);
+    }
+
+    @Test
+    public void testToMap(){
+        UserMapper mapper = getMapper();
+        Map<String, Object> UsertoMap = mapper.getUserToMap(1);
+        //注意：输出的结果，并不是按照User的属性 顺序的， 这体现了Map使用Hash 的无序性，根据Hash值存入元素
+        System.out.println(UsertoMap);
+    }
+
+
+    @Test
+    public void testGetAllUserToMap(){
+        UserMapper mapper = getMapper();
+        List<Map<String, Object>> allUserToMap = mapper.getAllUserToMap();
+        System.out.println(allUserToMap);
+    }
+
+    @Test
+    public void testGetAllUserToMap2(){
+        UserMapper mapper = getMapper();
+        Map allUserToMap = mapper.getAllUserToMap2();
+        System.out.println(allUserToMap);
+    }
+
+    @Test
+    public void testMohu(){
+        UserMapper mapper = getMapper();
+        List<User> users = mapper.getMohu("gmail");
+        System.out.println(users);
+    }
+
+    @Test
+    public void testDeleteMore(){
+        UserMapper mapper = getMapper();
+        //注意这种书写格式 id之间用逗号隔开
+        int i = mapper.deleteMore("3,4");
+        System.out.println(i);
+    }
+
+    @Test
+    public void testInsert(){
+        UserMapper mapper = getMapper();
+        User user = new User();
+        user.setUserName("Alex");
+        user.setAge(23);
+        user.setGender("男");
+        user.setPassWord("103377");
+       // user.setEmail(null);
+        int i = mapper.insertUserGeneratedKeys(user);
+        //注意，当插入成功后，自增主键的值会存回User 的id属性中
+        System.out.println("添加 "+i+ " 行记录 \n"+"新增记录 id= "+ user.getId());
+    }
+
 }
